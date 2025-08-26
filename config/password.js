c53b6090920b631f2bd8a0a529c0e7f512d5a02d
officialznkxproject-sys <officialznkxproject@gmail.com>
2025-08-26 20:59:36 +0700

    Create password.js

---
+++ config/password.js
@@ -0,0 +1,7 @@
+module.exports = {
+    ownerPasswords: {
+        '6283131871328': 'akusayangsusanti', // 083131871328
+        '6282181668718': 'akusayangtohang'   // 082181668718
+    },
+    verificationTokens: {}
+};
